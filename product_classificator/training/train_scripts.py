44d75830dd3a5bb2900977ce2ab6e77e941bdae7
SergeiSakh <sergei.sakhonenkov@gmail.com>
2024-05-21 00:27:49 +0300

    script for ruclip training added

--- product_classificator/training/train_scripts.py
+++ product_classificator/training/train_scripts.py
@@ -17,8 +17,8 @@ from ..ruclip.processor import RuCLIPProcessor
 from ..ruclip.ruclip_model import CLIP
 from .modules.cleaner import clean_dataset, TextCleaner
 from .modules.char_processor import CharExtractor, CharReducer
-from .modules.dataset import get_dataloaders
-from .modules.train_procedure import train_mlp_classifier
+from .modules.dataset import get_char_dataloaders, get_ruclip_dataloader
+from .modules.train_procedure import train_mlp_classifier, train_ruclip_one_epoch
 from .modules.visualisation import plot_history
 
 
@@ -52,7 +52,7 @@ class Trainer:
                  path_to_texts: str,
                  characteristics: list[str] = None,
                  task: Task = None,
-                 main_params: dict = None,
+                 heads_train_params: dict = None,
                  mlp_params: dict = None,
                  optim_params: dict = None,
                  min_products_in_sub_cat: int = 10,
@@ -74,7 +74,7 @@ class Trainer:
         self.heads_dir = heads_dir
         self.save_dir = save_dir
 
-        self.main_params = main_params or {
+        self.heads_train_params = heads_train_params or {
             'criterion': torch.nn.CrossEntropyLoss,
             'optimizer': torch.optim.Adam,
             'classificator': MLP,
@@ -121,7 +121,13 @@ class Trainer:
             self.logger.report_scalar(title=f'{char}: F1-macro', series=f'valid', value=history['valid_f1'][i],
                                       iteration=i + 1)
 
-    def loading_texts(self) -> pd.DataFrame:
+    def check_save_dir(self):
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        if not os.path.exists(os.path.join(self.save_dir, self.heads_dir)):
+            os.makedirs(os.path.join(self.save_dir, self.heads_dir))
+
+    def _loading_texts(self) -> pd.DataFrame:
         train = pd.read_parquet(self.path_to_dfs + self.wb_train_df)
         train = clean_dataset(train)
 
@@ -130,7 +136,7 @@ class Trainer:
 
         return pd.concat([train, test], ignore_index=True).reset_index(drop=True)
 
-    def preprocessing_texts(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _preprocessing_texts(self, df: pd.DataFrame) -> pd.DataFrame:
         train = df[df.characteristics.notna()]
         test = df[df.characteristics.isna()]
 
@@ -157,9 +163,6 @@ class Trainer:
         # объединение датасетов
         train_test = pd.concat([train, test], ignore_index=True).drop('title', axis=1)
 
-        # оставляем только ту часть датасета, что будет использоваться при обучении
-        train_test = train_test[train_test[self.characteristics].notna().any(axis=1)].reset_index(drop=True)
-
         cleaner = TextCleaner()
         self._show_info('Cleaning texts')
         with self.timer:
@@ -168,16 +171,15 @@ class Trainer:
                 self.task.register_artifact('Prepared data', train_test)
         self._show_info('End. Cleaning time: ' + str(self.timer.last_period))
 
+        train_test['nm'] = train_test['nm'].apply(lambda x: str(x) + '.jpg')
+
         return train_test
 
-    def run(self) -> None:
-
-        timer = Timer()
-
+    def _first_part(self, timer):
         self._show_info('Start text preprocessing')
         with timer:
-            train_test = self.loading_texts()
-            train_test = self.preprocessing_texts(train_test)
+            train_test = self._loading_texts()
+            train_test = self._preprocessing_texts(train_test)
         self._show_info('End. Preprocessing time: ' + str(timer.last_period))
 
         self._show_info('Start ruCLIP loading')
@@ -187,9 +189,49 @@ class Trainer:
         processor = RuCLIPProcessor.from_pretrained(self.cache_dir + self.ruclip_model)
         predictor = Predictor(clip, processor, device, quiet=True)
 
+        return train_test, clip, processor, predictor, device
+
+    def _train_heads_part(self, char_uniq, dataloaders, timer, save=True):
+        mlp_history = {char: {'train_loss': [], 'valid_loss': [], 'train_f1': [], 'valid_f1': []}
+                       for char in self.characteristics}
+
+        self._show_info('Start training MLP classifiers')
+        for char in self.characteristics:
+            mlp = MLP(in_channels=self.mlp_params['in_channels'],
+                      hidden_channels=[*self.mlp_params['hidden_channels'], char_uniq[char]],
+                      dropout=self.mlp_params['dropout'],
+                      activation_layer=self.mlp_params['activation'])
+
+            if self.task is not None:
+                self.task.connect({x.split(': ')[0].strip(): x.split(': ')[1] for x in str(mlp).split('\n')[1:-1]},
+                                  f'{char} MLP')
+
+            criterion = self.heads_train_params['criterion']()
+            optimizer = self.heads_train_params['optimizer'](mlp.parameters(), **self.optim_params)
+
+            with timer:
+                history, best_params = train_mlp_classifier(mlp, dataloaders[char], criterion, optimizer, char,
+                                                            epochs=self.heads_train_params['epochs'])
+            plot_history(history, char_name=char)
+
+            for key in mlp_history[char]:
+                mlp_history[char][key].append(max(history[key]))
+
+            self._show_info(f'End training MLP classifier "{char}". Time: ' + str(timer.last_period))
+            if self.task is not None:
+                self._log_history(char, history)
+
+            if save:
+                torch.save(best_params, os.path.join(self.save_dir, self.heads_dir, f'{char}.pt'))
+
+        return mlp_history
+
+    def _prepare_dataloaders_for_heads(self, train_test, predictor, embeddings=None):
         self._show_info('Preparing Data Loaders')
-        dataloaders = get_dataloaders(train_test, self.characteristics, predictor,
-                                      path_to_images=self.path_to_images, batch_size=self.main_params['batch_size'])
+        dataloaders = get_char_dataloaders(train_test, self.characteristics, predictor,
+                                           path_to_images=self.path_to_images,
+                                           embeddings=embeddings,
+                                           batch_size=self.heads_train_params['batch_size'])
 
         self._show_info('Saving a dict for transformation labels to characteristic values')
         label_to_char = {}
@@ -208,37 +250,64 @@ class Trainer:
 
         char_uniq = train_test[self.characteristics].nunique()
 
+        return dataloaders, char_uniq
+
+    def train_heads_only(self) -> None:
+
+        timer = Timer()
+        train_test, clip, processor, predictor, device = self._first_part(timer)
+
+        # оставляем только ту часть датасета, что будет использоваться при обучении
+        train_test = train_test[train_test[self.characteristics].notna().any(axis=1)].reset_index(drop=True)
+        dataloaders, char_uniq = self._prepare_dataloaders_for_heads(train_test, predictor)
+
         if self.task is not None:
             self.task.register_artifact('Num of unique characteristics', char_uniq.to_frame())
-            self.task.connect({k: str(v) for k, v in self.main_params.items()},
+            self.task.connect({k: str(v) for k, v in self.heads_train_params.items()},
                               name='Heads training parameters')
 
-        if not os.path.exists(self.save_dir):
-            os.makedirs(self.save_dir)
-        if not os.path.exists(os.path.join(self.save_dir, self.heads_dir)):
-            os.makedirs(os.path.join(self.save_dir, self.heads_dir))
-
-        self._show_info('Start training MLP classifiers')
-        for char in self.characteristics:
-            mlp = MLP(in_channels=self.mlp_params['in_channels'],
-                      hidden_channels=[*self.mlp_params['hidden_channels'], char_uniq[char]],
-                      dropout=self.mlp_params['dropout'],
-                      activation_layer=self.mlp_params['activation'])
-
-            criterion = self.main_params['criterion']()
-            optimizer = self.main_params['optimizer'](mlp.parameters(), **self.optim_params)
-
-            with timer:
-                history, best_params = train_mlp_classifier(mlp, dataloaders[char], criterion, optimizer, char,
-                                                            epochs=self.main_params['epochs'])
-            plot_history(history, char_name=char)
-
-            self._show_info(f'End training MLP classifier {char}. Time: ' + str(timer.last_period))
-            if self.task is not None:
-                self._log_history(char, history)
-
-            torch.save(best_params, os.path.join(self.save_dir, self.heads_dir, f'{char}.pt'))
+        self.check_save_dir()
+        self._train_heads_part(char_uniq, dataloaders, timer)
 
         if self.task is not None:
             self.task.close()
 
+    def train_ruclip(self, num_last_resblocks=2) -> None:
+
+        timer = Timer()
+        train_test, clip, processor, predictor, device = self._first_part(timer)
+
+        dataloader = get_ruclip_dataloader(train_test, processor, self.path_to_images)
+
+        self._freeze_parameters(clip, num_last_resblocks)
+
+        loss_img = torch.nn.CrossEntropyLoss()
+        loss_txt = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(clip.parameters(), lr=5e-5,
+                                     betas=(0.9, 0.98), eps=1e-6, weight_decay=0.2)
+
+        self.check_save_dir()
+        ruclip_loss = []
+        mlp_history = {char: {'train_loss': [], 'valid_loss': [], 'train_f1': [], 'valid_f1': []}
+                       for char in self.characteristics}
+
+        for epoch in range(1, self.heads_train_params['epochs'] + 1):
+            embeddings, loss = train_ruclip_one_epoch(clip, dataloader, loss_img, loss_txt, optimizer, device)
+            ruclip_loss.append(loss)
+
+            save = True if epoch == self.heads_train_params['epochs'] else False
+            dataloaders, char_uniq = self._prepare_dataloaders_for_heads(train_test, predictor, embeddings)
+            history = self._train_heads_part(char_uniq, dataloaders, timer, save=save)
+
+            for char in mlp_history:
+                for key in mlp_history[char]:
+                    mlp_history[char][key].append(history[key])
+
+    @staticmethod
+    def _freeze_parameters(clip: CLIP, num_last_resblocks: int) -> None:
+        for param in clip.parameters():
+            param.requires_grad = False
+
+        clip.visual.transformer.resblocks[-num_last_resblocks:].requires_grad = True
+        clip.visual.ln_post.requires_grad = True
+        clip.transformer.resblocks[-num_last_resblocks:].requires_grad = True
