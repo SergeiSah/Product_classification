66b7ba65be8dc64f6e9e360d78c7b6e6ba9c00d4
SergeiSakh <sergei.sakhonenkov@gmail.com>
2024-05-20 15:23:42 +0300

    char_uniq fixed

--- product_classificator/training/train_scripts.py
+++ product_classificator/training/train_scripts.py
@@ -59,7 +59,7 @@ class Trainer:
                  ruclip_model: str = 'ruclip-vit-base-patch16-384',
                  cache_dir: str = '/tmp/ruclip/',
                  heads_dir: str = 'heads/wb-6_cats',
-                 save_dir: str = ''):
+                 save_dir: str = '.'):
 
         self.task = task
 
@@ -121,73 +121,6 @@ class Trainer:
             self.logger.report_scalar(title=f'{char}: F1-macro', series=f'valid', value=history['valid_f1'][i],
                                       iteration=i + 1)
 
-    def run(self) -> None:
-
-        timer = Timer()
-
-        self._show_info('Start text preprocessing')
-        with timer:
-            train_test = self.loading_texts()
-            train_test = self.preprocessing_texts(train_test)
-        self._show_info('End. Preprocessing time: ' + str(timer.last_period))
-
-        self._show_info('Start ruCLIP loading')
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        load(self.ruclip_model, cache_dir=self.cache_dir)
-        clip = CLIP.from_pretrained(self.cache_dir + self.ruclip_model).eval().to(device)
-        processor = RuCLIPProcessor.from_pretrained(self.cache_dir + self.ruclip_model)
-        predictor = Predictor(clip, processor, device, quiet=True)
-
-        self._show_info('Preparing Data Loaders')
-        dataloaders = get_dataloaders(train_test, self.characteristics, predictor,
-                                      path_to_images=self.path_to_images, batch_size=self.main_params['batch_size'])
-
-        self._show_info('Saving a dict for transformation labels to characteristic values')
-        label_to_char = {}
-        for char in self.characteristics:
-            label_to_char[char] = dataloaders[char]['train'].dataset.label_to_char
-
-        if self.task is not None:
-            for char in label_to_char:
-                df = pd.DataFrame(
-                    {'label': list(label_to_char[char].keys()), char: list(label_to_char[char].values())}
-                )
-                self.task.register_artifact(char, df)
-
-        with open(os.path.join(self.save_dir, 'label_to_char.pkl'), 'wb') as f:
-            pickle.dump(label_to_char, f)
-
-        char_uniq = train_test[self.characteristics].nunique().to_frame()
-
-        if self.task is not None:
-            self.task.register_artifact('Num of unique characteristics', char_uniq)
-            self.task.connect({k: str(v) for k, v in self.main_params.items()},
-                              name='Heads training parameters')
-
-        self._show_info('Start training MLP classifiers')
-        for char in self.characteristics:
-            mlp = MLP(in_channels=self.mlp_params['in_channels'],
-                      hidden_channels=[*self.mlp_params['hidden_channels'], char_uniq[char]],
-                      dropout=self.mlp_params['dropout'],
-                      activation_layer=self.mlp_params['activation'])
-
-            criterion = self.main_params['criterion']()
-            optimizer = self.main_params['optimizer'](mlp.parameters(), **self.optim_params)
-
-            with timer:
-                history, best_params = train_mlp_classifier(mlp, dataloaders[char], criterion, optimizer, char,
-                                                            epochs=self.main_params['epochs'])
-            plot_history(history, char_name=char)
-
-            self._show_info(f'End training MLP classifier {char}. Time: ' + str(timer.last_period))
-            if self.task is not None:
-                self._log_history(char, history)
-
-            torch.save(best_params, os.path.join(self.save_dir, self.heads_dir, f'{char}.pt'))
-
-        if self.task is not None:
-            self.task.close()
-
     def loading_texts(self) -> pd.DataFrame:
         train = pd.read_parquet(self.path_to_dfs + self.wb_train_df)
         train = clean_dataset(train)
@@ -236,4 +169,76 @@ class Trainer:
         self._show_info('End. Cleaning time: ' + str(self.timer.last_period))
 
         return train_test
+
+    def run(self) -> None:
+
+        timer = Timer()
+
+        self._show_info('Start text preprocessing')
+        with timer:
+            train_test = self.loading_texts()
+            train_test = self.preprocessing_texts(train_test)
+        self._show_info('End. Preprocessing time: ' + str(timer.last_period))
+
+        self._show_info('Start ruCLIP loading')
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        load(self.ruclip_model, cache_dir=self.cache_dir)
+        clip = CLIP.from_pretrained(self.cache_dir + self.ruclip_model).eval().to(device)
+        processor = RuCLIPProcessor.from_pretrained(self.cache_dir + self.ruclip_model)
+        predictor = Predictor(clip, processor, device, quiet=True)
+
+        self._show_info('Preparing Data Loaders')
+        dataloaders = get_dataloaders(train_test, self.characteristics, predictor,
+                                      path_to_images=self.path_to_images, batch_size=self.main_params['batch_size'])
+
+        self._show_info('Saving a dict for transformation labels to characteristic values')
+        label_to_char = {}
+        for char in self.characteristics:
+            label_to_char[char] = dataloaders[char]['train'].dataset.label_to_char
+
+        if self.task is not None:
+            for char in label_to_char:
+                df = pd.DataFrame(
+                    {'label': list(label_to_char[char].keys()), char: list(label_to_char[char].values())}
+                )
+                self.task.register_artifact(char, df)
+
+        with open(os.path.join(self.save_dir, 'label_to_char.pkl'), 'wb') as f:
+            pickle.dump(label_to_char, f)
+
+        char_uniq = train_test[self.characteristics].nunique()
+
+        if self.task is not None:
+            self.task.register_artifact('Num of unique characteristics', char_uniq.to_frame())
+            self.task.connect({k: str(v) for k, v in self.main_params.items()},
+                              name='Heads training parameters')
+
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        if not os.path.exists(os.path.join(self.save_dir, self.heads_dir)):
+            os.makedirs(os.path.join(self.save_dir, self.heads_dir))
+
+        self._show_info('Start training MLP classifiers')
+        for char in self.characteristics:
+            mlp = MLP(in_channels=self.mlp_params['in_channels'],
+                      hidden_channels=[*self.mlp_params['hidden_channels'], char_uniq[char]],
+                      dropout=self.mlp_params['dropout'],
+                      activation_layer=self.mlp_params['activation'])
+
+            criterion = self.main_params['criterion']()
+            optimizer = self.main_params['optimizer'](mlp.parameters(), **self.optim_params)
+
+            with timer:
+                history, best_params = train_mlp_classifier(mlp, dataloaders[char], criterion, optimizer, char,
+                                                            epochs=self.main_params['epochs'])
+            plot_history(history, char_name=char)
+
+            self._show_info(f'End training MLP classifier {char}. Time: ' + str(timer.last_period))
+            if self.task is not None:
+                self._log_history(char, history)
+
+            torch.save(best_params, os.path.join(self.save_dir, self.heads_dir, f'{char}.pt'))
+
+        if self.task is not None:
+            self.task.close()
 
