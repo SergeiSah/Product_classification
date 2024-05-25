193f8684b4fe25c767b19dedca4544eb719c51dc
SergeiSakh <sergei.sakhonenkov@gmail.com>
2024-05-25 16:10:39 +0300

    test inference time

--- product_classificator/utils/benchmark.py
+++ product_classificator/utils/benchmark.py
@@ -1,3 +1,8 @@
+import os
+import gpustat
+import numpy as np
+import psutil
+import platform
 import torch
 import pandas as pd
 from more_itertools import chunked
@@ -12,28 +17,63 @@ class SpeedTest:
         self.clf = clf
         self.tests = []
 
-    def run_test(self,
-                 texts: list,
-                 images: list,
-                 batch_size: int,
-                 iterations_num: int = 1,
-                 empty_cuda_cache: bool = True) -> pd.DataFrame:
+        self.gpu_stats = gpustat.GPUStatCollection.new_query()
+
+        self.machine_info = {
+            'OS': platform.system(),
+            'CPU': platform.processor(),
+            'CPU cores': os.cpu_count(),
+            'GPUs': {idx: f"GPU {gpu.index}: {gpu.name} ({round(gpu.memory_total / 1024)} GB)"
+                     for idx, gpu in enumerate(self.gpu_stats)},
+            'GPU driver': self.gpu_stats.driver_version,
+            'RAM': f'{round(psutil.virtual_memory().total / 1024 ** 3)} GB',
+        }
+
+    def warming_up(self, text, image, warm_iterations):
+        with torch.no_grad():
+            for _ in tqdm(range(warm_iterations), desc='Warming up'):
+                self.clf.classify_products(text, image)
+
+    @staticmethod
+    def batches(texts, images, batch_size):
+        batches_num = len(texts) // batch_size
+
+        indexes = np.arange(len(texts))
+        np.random.shuffle(indexes)
+        indexes = indexes[:batches_num * batch_size]
+
+        texts = np.array(texts)[indexes]
+        images = [images[i] for i in indexes]
+
+        return zip(chunked(texts, batch_size), chunked(images, batch_size))
+
+    def test_inference(self,
+                       texts: list,
+                       images: list,
+                       batch_size: int,
+                       iterations_num: int = 1,
+                       warm_iterations: int = 0,
+                       empty_cuda_cache: bool = True) -> pd.DataFrame:
 
         log = pd.DataFrame(
             columns=[f'iter_{i}' for i in range(iterations_num)],
-            index=list(range(len(texts) // batch_size))
+            index=list(range(len(texts) // batch_size)),
+            dtype='float64'
         )
 
         timer = Timer()
+
+        if warm_iterations > 0:
+            self.warming_up(texts[0], images[0], warm_iterations)
 
         if empty_cuda_cache:
             torch.cuda.empty_cache()
 
         with torch.no_grad():
             for i in range(iterations_num):
-                for j, (texts_batch, images_batch) in tqdm(enumerate(zip(chunked(texts, batch_size),
-                                                                         chunked(images, batch_size))),
-                                                           total=len(texts) // batch_size):
+                for j, (texts_batch, images_batch) in tqdm(enumerate(self.batches(texts, images, batch_size)),
+                                                           total=len(texts) // batch_size,
+                                                           desc='Inference'):
                     with timer:
                         self.clf.classify_products(texts_batch, images_batch)
                     log.loc[j, f'iter_{i}'] = timer.last_period.total_seconds()
@@ -51,3 +91,4 @@ class SpeedTest:
         })
 
         return log
+
