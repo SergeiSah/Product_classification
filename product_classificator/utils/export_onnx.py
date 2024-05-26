4f0a49825804d6beed257c8e1750329b2d26fbc7
SergeiSakh <sergei.sakhonenkov@gmail.com>
2024-05-26 12:43:20 +0300

    onnx export params

--- product_classificator/utils/export_onnx.py
+++ product_classificator/utils/export_onnx.py
@@ -7,8 +7,7 @@ from PIL import Image
 def export_onnx(clf,
                 text,
                 image,
-                opset_version: int,
-                dynamic_axes: dict):
+                params: dict):
 
     assert isinstance(text, str)
     assert isinstance(image, Image.Image)
@@ -19,25 +18,19 @@ def export_onnx(clf,
     folder = os.path.join(clf.cache_dir, clf.model_name)
 
     dummy = processor(text=[text], images=[image], return_tensors='pt', padding=True)
-    dummy_text = clip.token_embedding(dummy['input_ids']).type(clip.dtype).permute(1, 0, 2)
-    dummy_image = dummy['pixel_values']
+    dummy_text = clip.token_embedding(dummy['input_ids'].to(clf.clip_predictor.device)).type(clip.dtype).permute(1, 0, 2)
+    dummy_image = dummy['pixel_values'].to(clf.clip_predictor.device)
 
     torch.onnx.export(
         clip.visual,
         dummy_image,
         os.path.join(folder, "clip_visual.onnx"),
-        input_names=['input'],
-        output_names=['output'],
-        opset_version=opset_version,
-        dynamic_axes=dynamic_axes
+        **params
     )
 
     torch.onnx.export(
         clip.transformer,
-        dummy_text,
+        dummy_text.to(clf.clip_predictor.device),
         os.path.join(folder, "clip_transformer.onnx"),
-        input_names=['input'],
-        output_names=['output'],
-        opset_version=opset_version,
-        dynamic_axes=dynamic_axes
+        **params
     )
